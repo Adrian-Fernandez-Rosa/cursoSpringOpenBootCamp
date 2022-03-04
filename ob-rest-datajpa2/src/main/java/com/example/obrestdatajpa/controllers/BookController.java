7dd575283f884038553b3d22b3f26285b2d6435c
adrianprogama <adrianfr2011@gmail.com>
2022-03-03 21:01:54 -0300

    sesion 5 api rest

--- ob-rest-datajpa2/src/main/java/com/example/obrestdatajpa/controllers/BookController.java
+++ ob-rest-datajpa2/src/main/java/com/example/obrestdatajpa/controllers/BookController.java
@@ -2,10 +2,12 @@ package com.example.obrestdatajpa.controllers;
 
 import com.example.obrestdatajpa.entidades.Book;
 import com.example.obrestdatajpa.repository.BookRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -29,18 +31,50 @@ public class BookController {
         return bookRepository.findAll();
     }
 
-    //buscar un solo libro segun su id
+    //buscar un libro segun su id
     /*
-    public Book findOneById(Long id){
+    @GetMapping("/api/books/{id}")
+    public Book findOneById(@PathVariable Long id){ //recordar que Path... vincula con la url
+        //normalmente se devuelve response entity
+        Optional<Book> bookOpt = bookRepository.findById(id);
 
-        return bookRepository.findById(id);
+        return bookOpt.orElse(null);
+    }
+ */
+
+    /**
+     * Request
+     * Response
+     * @param id
+     * @return
+     */
+    @GetMapping("/api/books/{id}")
+    public ResponseEntity<Book> findOneById(@PathVariable Long id){ //recordar que Path... vincula con la url
+        //normalmente se devuelve response entity
+        Optional<Book> bookOpt = bookRepository.findById(id);
+
+         if(bookOpt.isPresent())
+             return ResponseEntity.ok(bookOpt.get());
+         else
+             return ResponseEntity.notFound().build();
+         //Construye response entity 404
+
+        // return bookOpt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-     */
+
+
 
 
     //crear un nuevo libro
 
+    @PostMapping("/api/books")
+    public Book create(@RequestBody Book book, @RequestHeader HttpHeaders headers){
+        //@RequestBody  extrae la información de la petición y cargala en un parametro de tipo libro
+
+        System.out.println(headers.get("User-Agent"));
+        return bookRepository.save(book);
+    }
 
     // actualizar un libro existente
 
